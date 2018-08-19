9be2b3dd1b78b3962c8d0faa884403cddf758a6d
Sergei.Belonosov <2belonosov@gmail.com>
2018-08-19 19:24:22 +0700

    FIX: Model classes were refactored.

--- src/main/java/com/mdevi/bookmongo/model/Comment.java
+++ src/main/java/com/mdevi/bookmongo/model/Comment.java
@@ -1,4 +1,50 @@
 package com.mdevi.bookmongo.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.sql.Date;
+
+@Document
 public class Comment {
+
+    @Field("observer")
+    private String commenter;
+    @Field("text")
+    private String text;
+    @Field("create_on")
+    private Date date;
+
+    public Comment() {
+    }
+
+    public Comment(String commenter, String text, Date date) {
+        this.commenter = commenter;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getCommenter() {
+        return commenter;
+    }
+
+    public void setCommenter(String commenter) {
+        this.commenter = commenter;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }

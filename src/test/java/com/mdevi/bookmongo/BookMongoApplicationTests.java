8dddb4e995fe8362aa17d2a253b3ba69598d0d41
Sergei.Belonosov <2belonosov@gmail.com>
2018-08-20 12:07:59 +0700

    ADD:  App properties, ADD: mongobackup.zip as Mongo DB dump for collection books. ADD: books-sample.json - sample books data.

--- src/test/java/com/mdevi/bookmongo/BookMongoApplicationTests.java
+++ src/test/java/com/mdevi/bookmongo/BookMongoApplicationTests.java
@@ -3,10 +3,15 @@ package com.mdevi.bookmongo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "= false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"})
+@ActiveProfiles("test")
 public class BookMongoApplicationTests {
 
     @Test
